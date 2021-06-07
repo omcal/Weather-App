8bdc954eacb99dc16f5d102bed34520c576f50ff
omerisildak <omerisildak0@gmail.com>
2021-06-07 16:38:39 +0300

    script.js and updates of index,stlye

---
+++ script.js
@@ -0,0 +1,54 @@
+let weather = {
+    apiKey: "Api Key",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        )
+            .then((response) => {
+                if (!response.ok) {
+                    alert("Hava Durumu Bulunamadı");
+                    throw new Error("Hava Durumu Bulunamadı");
+                }
+                return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        document.querySelector(".city").innerText = name+"   Hava Durumu ";
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText =
+            "Nem: " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Rüzgar Hızı: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("yükleniyor");
+        document.body.style.backgroundImage =
+            "url('https://source.unsplash.com/1600x1000/?" + name + "')";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
+
+weather.fetchWeather("Ankara");
